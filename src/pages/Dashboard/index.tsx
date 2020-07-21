bb3530ea3c788d6a85c33abe01e675808757e9a3
Bernardo Henrique <bernardohrl.unb@gmail.com>
2020-07-21 20:11:31 -0300

    Styles Dashboard
    - Adds Polished libary to handle collors.
    - Adds React Icons

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,60 @@
 import React from "react";
-import { Title } from "./styles";
+import { FiChevronRight } from "react-icons/fi";
+
+import logo from "../../assets/logo.svg";
+import { Title, Form, Repositories } from "./styles";
 
 // FC = FunctionComponent
 const Dashboard: React.FC = () => {
-    return <Title>Explore Repositórios do Github</Title>;
+    return (
+        <>
+            <img src={logo} alt="Github Explorer" />
+            <Title>Explore Repositórios do Github</Title>
+
+            <Form>
+                <input placeholder="Digite o nome do repositório" />
+                <button type="submit"> Pesquisar </button>
+            </Form>
+
+            <Repositories>
+                <a href="test">
+                    <img
+                        src="https://avatars2.githubusercontent.com/u/20937488?s=460&u=5e8d3b43e8eb6c38562caf172224e972b5bc9856&v=4"
+                        alt="User Profile"
+                    />
+                    <div>
+                        <strong> Github Explorer </strong>
+                        <p> Page to explore github repositories </p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+
+                <a href="test">
+                    <img
+                        src="https://avatars2.githubusercontent.com/u/20937488?s=460&u=5e8d3b43e8eb6c38562caf172224e972b5bc9856&v=4"
+                        alt="User Profile"
+                    />
+                    <div>
+                        <strong> Github Explorer </strong>
+                        <p> Page to explore github repositories </p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+
+                <a href="test">
+                    <img
+                        src="https://avatars2.githubusercontent.com/u/20937488?s=460&u=5e8d3b43e8eb6c38562caf172224e972b5bc9856&v=4"
+                        alt="User Profile"
+                    />
+                    <div>
+                        <strong> Github Explorer </strong>
+                        <p> Page to explore github repositories </p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+            </Repositories>
+        </>
+    );
 };
 
 export default Dashboard;
