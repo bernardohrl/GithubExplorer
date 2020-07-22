03ba1aebcf82ef159f5ed1b99bc88842573f614f
Bernardo Henrique <bernardohrl.unb@gmail.com>
2020-07-22 19:49:39 -0300

    Styles Repository Detail Page

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+import logo from '../../assets/logo.svg';
 
 interface RepositoryParams {
     repository: string;
@@ -9,7 +13,55 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
     const { params } = useRouteMatch<RepositoryParams>();
 
-    return <h1>{params.repository}</h1>;
+    return (
+        <>
+            <Header>
+                <img src={logo} alt="Github Explorer" />
+
+                <Link to="/">
+                    <FiChevronLeft size={16} />
+                    Voltar
+                </Link>
+            </Header>
+
+            <RepositoryInfo>
+                <header>
+                    <img
+                        src="https://avatars2.githubusercontent.com/u/20937488?s=460&u=5e8d3b43e8eb6c38562caf172224e972b5bc9856&v=4"
+                        alt="User"
+                    />
+                    <div>
+                        <strong>gobarber/frontend</strong>
+                        <p>Repositório para fazer várias coisas e etc</p>
+                    </div>
+                </header>
+                <ul>
+                    <li>
+                        <strong> 1808 </strong>
+                        <span>Stars</span>
+                    </li>
+                    <li>
+                        <strong> 50 </strong>
+                        <span>Forks</span>
+                    </li>
+                    <li>
+                        <strong> 92 </strong>
+                        <span>Issues Abertas</span>
+                    </li>
+                </ul>
+            </RepositoryInfo>
+
+            <Issues>
+                <Link to="?">
+                    <div>
+                        <strong> Nome da Issue </strong>
+                        <p> Descrição da Issue </p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </Link>
+            </Issues>
+        </>
+    );
 };
 
 export default Repository;
